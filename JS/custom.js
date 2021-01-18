bbbf4df69ced3e94243e105b69fd2cdfc4aac1f8
Neil-seu <Neil-seu@users.noreply.github.com>
2021-01-19 02:06:48 +0600

    Add files via upload

---
+++ JS/custom.js
@@ -0,0 +1,74 @@
+$('#testimonialCarousel').carousel({
+    interval: 2000
+  })
+  
+  $('.carousel .carousel-item').each(function(){
+      var minPerSlide = 1;
+      var next = $(this).next();
+      if (!next.length) {
+      next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+      
+      for (var i=0;i<minPerSlide;i++) {
+          next=next.next();
+          if (!next.length) {
+              next = $(this).siblings(':first');
+            }
+          
+          next.children(':first-child').clone().appendTo($(this));
+      }
+  });
+  
+  $('.portfolio-item').isotope ({
+    itemSelector: '.card',
+    layoutMode: 'fitRows',
+  });
+  $('.portfolioMainLinks a').click(function() {
+    $('.portfolioMainLinks a').removeClass('active');
+    $(this).addClass('active');
+    
+    var selector = $(this).attr('data-filter');
+    $('.portfolio-item').isotope({
+      filter: selector
+    });
+    return false;
+  });
+$(window).load(function() {
+    equalheight('.card');
+});
+
+equalheight = function(container){
+
+    var currentTallest = 0,
+    currentRowStart = 0,
+    rowDivs = new Array(),
+    $el,
+    topPosition = 0;
+    $(container).each(function() {
+
+        $el = $(this);
+        $($el).height('auto')
+        topPostion = $el.position().top;
+
+        if (currentRowStart != topPostion) {
+            for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                rowDivs[currentDiv].height(currentTallest);
+            }
+        rowDivs.length = 0; // empty the array
+        currentRowStart = topPostion;
+        currentTallest = $el.height();
+        rowDivs.push($el);
+        } else {
+            rowDivs.push($el);
+            currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+        }
+        for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+            rowDivs[currentDiv].height(currentTallest);
+        }
+    });
+}
+
+$(window).resize(function(){
+  equalheight('.card');
+});
